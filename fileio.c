6db92cf385a51490587b614503f9e0b2aa1811c0
Patrick Taylor <pst7@students.uwf.edu>
2016-04-19 17:24:58 -0500

    current work as of 04.19.16 - 17.25

--- fileio.c
+++ fileio.c
@@ -1,18 +1,29 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "users.h"
+#include "linkedList.h"
 
 char exists(char *fn);
 void initDataFile();
 void readFile(char *fn);
+char getData(char *input, int count);
 
 int main()
 {
     char *fn = "data.dat";
+    int count = 0;
     while(!exists(fn))
         initDataFile(fn);
     readFile(fn);
     printf("\n");
+    getData("PAT",count++);
+    getData("1234",count++);
+    getData("name",count++);
+    printf("\n");
     return 0;
 }
+// true if data file exists
 char exists(char *fn)
 {
     FILE *fp = fopen(fn,"r");
@@ -21,12 +32,14 @@ char exists(char *fn)
     fclose(fp);
     return 1;
 }
+// creates new data file
 void initDataFile(char *fn)
 {
     FILE *fp = fopen(fn,"w");
     fprintf(fp,"CONTENTS OF DATA FILE");
     fclose(fp);
 }
+// reads contents of data file
 void readFile(char *fn)
 {
     char buffer = 0;
@@ -38,4 +51,30 @@ void readFile(char *fn)
             putchar(buffer);
     }
     fclose(fp);
+}
+char getData(char *input, int count)
+{
+    if(count==0)
+    {
+        if(!strncmp(input,"pat",3))
+        {
+            return 1;
+        }
+    }  
+    else if(count==1)
+    {
+        if(!strncmp(input,"1234",4))
+        {
+            return 1;
+        }
+    }
+    else if(count==2)
+    {
+        if(!strncmp(input,"name",4))
+        {
+            printf("Patrick");
+            return 1;
+        }
+    }
+    return 0;
 }
--- linkedList.h
+++ linkedList.h
@@ -8,16 +8,15 @@ void addNode(node_t node, char *val);
 
 node_t initList()
 {
-	node_t * head = NULL;
+	node_t *head = NULL;
 	head = malloc(sizeof(node_t));
-	if (head == NULL)
-	    return 1;
 	head->val = NULL;
 	head->next = NULL;
+	return *head;
 }
-void addNode(node_t n, char *val);
+void addNode(node_t n, char *val)
 {
-	n->next = malloc(sizeof(node_t));
-	n->next->val = val;
-	n->next->next = NULL;
+	n.next = malloc(sizeof(node_t));
+	n.next->val = val;
+	n.next->next = NULL;
 }
