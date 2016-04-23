70d3d6d2292c51e35f1c91c3a71b5e72dc2e2f2a
Lance Fisher <ld.fisher@outlook.com>
2016-04-22 23:04:49 -0500

    Added SHA1 capabilities to auth, token creation finished.

---
+++ auth.c
@@ -0,0 +1,81 @@
+/*
+use : Authentication
+auth: L Fisher
+date: 15 APR 2016
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "auth.h"
+#include "sha1.h"
+
+
+
+
+
+
+
+int main() {
+
+	unsigned authToken[5] = {0, 0, 0, 0, 0};
+	int i = 0;
+
+	int count = 0;
+
+	char* userString = "Lance Fisher";
+	char* passString = "Test123$%^";
+
+	unsigned userLen = 0;
+	unsigned passLen = 0;
+	while (userString[count++] != 0) userLen++;
+	count = 0;
+	while (passString[count++] != 0) passLen++;
+
+	SHA1Context* p = malloc(sizeof(SHA1Context));
+
+	SHA1Reset(p);
+	SHA1Input(p, userString, userLen);
+	SHA1Result(p);
+
+	printf("%20s", "USER HASH : ");
+
+	for (i = 0; i < 5; i++)
+		printf("%08X ", p->Message_Digest[i]);
+	printf("\n");
+
+
+	for (i = 0; i < 5; i++) authToken[i] ^= p->Message_Digest[i];
+
+
+
+//
+
+	SHA1Reset(p);
+	SHA1Input(p, passString, passLen);
+	SHA1Result(p);
+
+	
+	printf("%20s", "PWD HASH : ");
+
+	for (i = 0; i < 5; i++)
+		printf("%08X ", p->Message_Digest[i]);
+	printf("\n");
+
+	for (i = 0; i < 5; i++) authToken[i] ^= p->Message_Digest[i];
+
+
+
+
+	printf("%20s", "AUTH TOKEN (XOR) : ");
+
+	for (i = 0; i < 5; i++)
+		printf("%08X ", authToken[i]);
+	printf("\n");
+
+
+
+
+}
+
---
+++ auth.h
@@ -0,0 +1,17 @@
+#ifndef AUTH_H
+#define AUTH_H
+
+
+unsigned char *SHA1(const unsigned char *d, size_t n, unsigned char *md);
+
+
+
+
+
+
+
+
+
+
+
+#endif
