9b65bace8494df1d06c642d6492340f47db40b37
Patrick Taylor <pst7@students.uwf.edu>
2016-04-26 02:21:44 -0500

    all functions except for sendImmunizations are fully operational

--- src/fileio-funcdecs.h
+++ src/fileio-funcdecs.h
@@ -2,19 +2,27 @@
 #define SIZEOF_FILENUMBER 2
 
 /*
- * char patientid[3], unique ID of patient
+ * unsigned int patientid, unique ID of patient
  * char fileType,
  *     1-inprocessing
  *     2-outprocessing
  *     3-immunizations
  *     4-medications
- * char fileNumber, number of file in database
+ * unsigned int fileNumber, number of file in database
  */
-void readFile(char patientid[SIZEOF_PATIENTID],char fileType,char fileNumber[SIZEOF_FILENUMBER]);
-void createFile(char patientid[SIZEOF_PATIENTID],char fileType,char fileNumber[SIZEOF_FILENUMBER]);
-void deleteFile(char patientid[SIZEOF_PATIENTID],char fileType,char fileNumber[SIZEOF_FILENUMBER]);
+void readFile(unsigned int patientid,char fileType,unsigned int fileNumber);
+void createFile(unsigned int patientid,char fileType,unsigned int fileNumber);
+void deleteFile(unsigned int patientid,char fileType,unsigned int fileNumber);
 
-void sendImmmunizations(char patientid[SIZEOF_PATIENTID]); // moves immunizations files to immunizationClinic directory
+/*
+ * char *data
+ *     data to be written to file
+ */
+void editFile(char *data,unsigned int patientid,char fileType,unsigned int fileNumber);
+void appendFile(char *data,unsigned int patientid,char fileType,unsigned int fileNumber);
+
+void sendImmmunizations(unsigned int patientid); // moves immunizations files to immunizationClinic directory
 
 // helper function(s)
-void concatFilePath(char *filePath,char patientid[SIZEOF_PATIENTID],char fileType,char fileNumber[SIZEOF_FILENUMBER]);
+void concatFilePath(char *filePath,unsigned int patientid,char fileType,unsigned int fileNumber); // combines params to generate filePath
+char fileExists(char *filePath); // returns 1 if file exists
--- src/fileio.c
+++ src/fileio.c
@@ -10,36 +10,84 @@
 int main()
 {
     printf("----Start of FileIO----\n");
-    readFile("000",'1',"00"); // function tester
-    printf("\n---- End of FileIO ----\n");
+    createFile(000,'1',0); // function tester
+    readFile(000,'1',00); // function tester
+    editFile("This is some data from the user",000,'1',00); // function tester
+    readFile(000,'1',00); // function tester
+    appendFile("--Let\'s append some more data!",000,'1',00); // function tester
+    readFile(000,'1',00); // function tester
+    deleteFile(000,'1',0); // function tester
+    // sendImmmunizations(000); // function tester
+    printf("---- End of FileIO ----\n");
     return 0;
 }
 
-void readFile(char patientid[SIZEOF_PATIENTID],char fileType,char fileNumber[SIZEOF_FILENUMBER])
+void readFile(unsigned int patientid,char fileType,unsigned int fileNumber)
 {
     char *filePath = malloc(1000); // buffer for filePath
     concatFilePath(filePath,patientid,fileType,fileNumber); // generate filePath using params
-    FILE *filePointer = fopen(filePath,"r"); // open file
-    if(filePointer==NULL) fprintf(stderr, "NULL filePointer"); // check for null pointer
-    char c;
-    while((c=fgetc(filePointer))!=EOF) printf("%c",c); // output char by char to EOF
-    fclose(filePointer); // close file
+    if(fileExists(filePath))
+    {
+        FILE *filePointer = fopen(filePath,"r");
+        char c;
+        while((c=fgetc(filePointer))!=EOF) printf("%c",c); // output char by char to EOF
+        fclose(filePointer); // close file
+    }
+    else fprintf(stderr,"file \"%s\" does not exist\n",filePath);
     free(filePath); // free filePath malloc
+    printf("\n");
 }
-void createFile(char patientid[SIZEOF_PATIENTID],char fileType,char fileNumber[SIZEOF_FILENUMBER])
+void createFile(unsigned int patientid,char fileType,unsigned int fileNumber)
+{
+    char *filePath = malloc(1000); // buffer for filePath
+    concatFilePath(filePath,patientid,fileType,fileNumber); // generate filePath using params
+    FILE *filePointer = fopen(filePath,"w");
+    if(fileExists(filePath))
+    {
+        fprintf(filePointer,"default contents of \"%s\"",filePath);
+        fclose(filePointer);
+        printf("\"%s\" created w/o issue\n",filePath);
+    }
+    else fprintf(stderr, "unable to create \"%s\"\n", filePath);
+}
+void deleteFile(unsigned int patientid,char fileType,unsigned int fileNumber)
+{
+    char *filePath = malloc(1000); // buffer for filePath
+    concatFilePath(filePath,patientid,fileType,fileNumber); // generate filePath using params
+    if(remove(filePath)) fprintf(stderr, "unable to delete file\n");
+    else fprintf(stderr, "\"%s\" deleted\n", filePath);
+}
+void editFile(char *data,unsigned int patientid,char fileType,unsigned int fileNumber)
+{
+    char *filePath = malloc(1000); // buffer for filePath
+    concatFilePath(filePath,patientid,fileType,fileNumber); // generate filePath using params
+    FILE *filePointer = fopen(filePath,"w");
+    if(fileExists(filePath))
+    {
+        fprintf(filePointer,data);
+        fclose(filePointer);
+        printf("\"%s\" written to \"%s\" w/o issue\n",data,filePath);
+    }
+    else fprintf(stderr, "unable to create \"%s\"\n", filePath);
+}
+void appendFile(char *data,unsigned int patientid,char fileType,unsigned int fileNumber)
+{
+    char *filePath = malloc(1000); // buffer for filePath
+    concatFilePath(filePath,patientid,fileType,fileNumber); // generate filePath using params
+    FILE *filePointer = fopen(filePath,"a");
+    if(fileExists(filePath))
+    {
+        fprintf(filePointer,data);
+        fclose(filePointer);
+        printf("\"%s\" appended to \"%s\" w/o issue\n",data,filePath);
+    }
+    else fprintf(stderr, "unable to create \"%s\"\n", filePath);   
+}
+void sendImmmunizations(unsigned int patientid)
 {
 
 }
-void deleteFile(char patientid[SIZEOF_PATIENTID],char fileType,char fileNumber[SIZEOF_FILENUMBER])
-{
-
-}
-void sendImmmunizations(char patientid[SIZEOF_PATIENTID])
-{
-
-}
-
-void concatFilePath(char *filePath,char patientid[SIZEOF_PATIENTID],char fileType,char fileNumber[SIZEOF_FILENUMBER])
+void concatFilePath(char *filePath,unsigned int patientid,char fileType,unsigned int fileNumber)
 {   
     char *filePathRoot = ".././patients/"; // basic root for all patient files
     char *fileTypeExplicit; // container for explicit name of fileType
@@ -47,7 +95,18 @@ void concatFilePath(char *filePath,char patientid[SIZEOF_PATIENTID],char fileTyp
     else if(fileType=='2') fileTypeExplicit="outprocessing";
     else if(fileType=='3') fileTypeExplicit="immunizations";
     else if(fileType=='4') fileTypeExplicit="medications";
-    else fprintf(stderr, "incorrect fileType");
-    sprintf(filePath,"%s%s%c%s%s%s",filePathRoot,patientid,'/',patientid,fileTypeExplicit,fileNumber); // concatenate filePath
+    else fprintf(stderr, "incorrect fileType\n");
+    sprintf(filePath,"%s%03d%c%03d%s%02d",filePathRoot,patientid,'/',patientid,fileTypeExplicit,fileNumber); // concatenate filePath
     // printf("%s",filePath);
+}
+char fileExists(char *filePath)
+{
+    FILE *filePointer = fopen(filePath,"r"); // try to open file
+    if(filePointer==NULL) // check for null pointer
+    {
+        fprintf(stderr, "NULL filePointer\n");
+        return FALSE; 
+    }
+    fclose(filePointer); // close file
+    return TRUE;
 }
