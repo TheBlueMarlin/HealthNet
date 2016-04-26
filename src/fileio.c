77a7d1618fc01545ce728dd65276c057d8d4ae80
Patrick Taylor <pst7@students.uwf.edu>
2016-04-25 19:02:21 -0500

    added instantiated functions below main function & removed extra newlines in template of maino

--- src/fileio.c
+++ src/fileio.c
@@ -17,7 +17,45 @@ void deleteRecord(char *username);
 
 int main()
 {
-    printf("\n----Start of FileIO----\n");
-    printf("\n---- End of FileIO ----\n");
+    printf("----Start of FileIO----\n");
+
+    printf("---- End of FileIO ----\n");
     return 0;
+}
+
+void createAccount(char role, char *username, char *password) {
+
+}
+void assignPatient(char *parent, char *patient) {
+
+}
+void unassignPatient(char *parent, char *patient) {
+
+}
+void viewAssignments(char *username) {
+
+}
+char getRole(char *username) {
+    return 0;
+}
+char userExists(char *username) {
+    return 0;
+}
+void deleteAccount(char *username) {
+
+}
+void createRecord(char *username) {
+
+}
+void editRecordField(char *username, char field) {
+
+}
+void viewRecordField(char *username, char field) {
+
+}
+void viewRecord(char *username) {
+
+}
+void deleteRecord(char *username) {
+
 }
