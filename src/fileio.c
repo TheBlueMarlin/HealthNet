c593375629c319463d54fa1f1d449a824638f392
Patrick Taylor <pst7@students.uwf.edu>
2016-04-26 03:29:46 -0500

    still working on finishing the sendImmunizations functionality

--- src/fileio.c
+++ src/fileio.c
@@ -7,16 +7,26 @@
 #define TRUE 1
 #define FALSE 0
 
+#define SIZEOF_PATIENTID 3
+#define SIZEOF_FILENUMBER 2
+
+#define FILETYPE_INPROCESSING '1'
+#define FILETYPE_OUTPROCESSING '2'
+#define FILETYPE_IMMUNIZATIONS '3'
+#define FILETYPE_MEDICATIONS '4'
+
+#define NARROWPATH_OFFSET 14 // ".././patients/"
+
 int main()
 {
     printf("----Start of FileIO----\n");
-    createFile(000,'1',0); // function tester
-    readFile(000,'1',00); // function tester
-    editFile("This is some data from the user",000,'1',00); // function tester
-    readFile(000,'1',00); // function tester
-    appendFile("--Let\'s append some more data!",000,'1',00); // function tester
-    readFile(000,'1',00); // function tester
-    deleteFile(000,'1',0); // function tester
+    createFile(0,FILETYPE_INPROCESSING,0); // function tester
+    readFile(0,FILETYPE_INPROCESSING,0); // function tester
+    editFile("This is some data from the user",0,FILETYPE_INPROCESSING,0); // function tester
+    readFile(0,FILETYPE_INPROCESSING,0); // function tester
+    appendFile("--Let\'s append some more data!",0,FILETYPE_INPROCESSING,0); // function tester
+    readFile(0,FILETYPE_INPROCESSING,0); // function tester
+    deleteFile(0,FILETYPE_INPROCESSING,0); // function tester
     // sendImmmunizations(000); // function tester
     printf("---- End of FileIO ----\n");
     return 0;
@@ -84,19 +94,41 @@ void appendFile(char *data,unsigned int patientid,char fileType,unsigned int fil
     else fprintf(stderr, "unable to create \"%s\"\n", filePath);   
 }
 void sendImmmunizations(unsigned int patientid)
-{
-
-}
+{/*
+    char *filePath = malloc(1000); // buffer for filePath, orig location of record
+    char *movePath = malloc(1000); // buffer for movePath, new location for record
+    char *narrowPath = malloc(1000); // buffer for narrowPath, path w/in orig root directory
+    unsigned int i = 0; // general index
+    unsigned int x = 0; // general index
+    unsigned int immunizations = 0; // number of immunization files
+    concatFilePath(filePath,patientid,'FILETYPE_IMMUNIZATIONS',immunizations); // generate initial filePath using params
+    while(fileExists(filePath))
+    {
+        i=0;
+        x=NARROWPATH_OFFSET-1;
+        while(x<sizeof(filePath))
+        {
+            narrowPath[i] = filePath[x++]
+        }
+        sprintf(movePath,".././immunizationsclinic/%s",narrowPath);
+        if(rename(filePath,movePath)) fprintf(stderr, "\"%s\" unable to be moved to \"%s\"",filePath,movePath); // copy file to movePath
+        else deleteFile(filePath); // if successful, delete original file
+        concatFilePath(filePath,patientid,'FILETYPE_IMMUNIZATIONS',++immunizations); // generate initial filePath using params
+    }
+    free(filePath);
+    free(movePath);
+    free(narrowPath);
+*/}
 void concatFilePath(char *filePath,unsigned int patientid,char fileType,unsigned int fileNumber)
 {   
     char *filePathRoot = ".././patients/"; // basic root for all patient files
     char *fileTypeExplicit; // container for explicit name of fileType
-    if(fileType=='1') fileTypeExplicit="inprocessing";
-    else if(fileType=='2') fileTypeExplicit="outprocessing";
-    else if(fileType=='3') fileTypeExplicit="immunizations";
-    else if(fileType=='4') fileTypeExplicit="medications";
+    if(fileType==FILETYPE_INPROCESSING) fileTypeExplicit="inprocessing";
+    else if(fileType==FILETYPE_OUTPROCESSING) fileTypeExplicit="outprocessing";
+    else if(fileType==FILETYPE_IMMUNIZATIONS) fileTypeExplicit="immunizations";
+    else if(fileType==FILETYPE_MEDICATIONS) fileTypeExplicit="medications";
     else fprintf(stderr, "incorrect fileType\n");
-    sprintf(filePath,"%s%03d%c%03d%s%02d",filePathRoot,patientid,'/',patientid,fileTypeExplicit,fileNumber); // concatenate filePath
+    sprintf(filePath,"%s%03d/%03d%s%02d",filePathRoot,patientid,patientid,fileTypeExplicit,fileNumber); // concatenate filePath
     // printf("%s",filePath);
 }
 char fileExists(char *filePath)
