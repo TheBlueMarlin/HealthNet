08c66b11b9386344c62a21550ed7a47367a44706
Patrick Taylor <pst7@students.uwf.edu>
2016-04-25 19:09:45 -0500

    moved fileio function declarations to "fileio-funcdecs.h" and included it in "fileio.c"

---
+++ src/fileio-funcdecs.h
@@ -0,0 +1,12 @@
+void createAccount(char role, char *username, char *password);
+void assignPatient(char *parent, char *patient);
+void unassignPatient(char *parent, char *patient);
+void viewAssignments(char *username);
+char getRole(char *username);
+char userExists(char *username);
+void deleteAccount(char *username);
+void createRecord(char *username);
+void editRecordField(char *username, char field);
+void viewRecordField(char *username, char field);
+void viewRecord(char *username);
+void deleteRecord(char *username);
--- src/fileio.c
+++ src/fileio.c
@@ -2,18 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 
-void createAccount(char role, char *username, char *password);
-void assignPatient(char *parent, char *patient);
-void unassignPatient(char *parent, char *patient);
-void viewAssignments(char *username);
-char getRole(char *username);
-char userExists(char *username);
-void deleteAccount(char *username);
-void createRecord(char *username);
-void editRecordField(char *username, char field);
-void viewRecordField(char *username, char field);
-void viewRecord(char *username);
-void deleteRecord(char *username);
+#include "fileio-funcdecs.h"
 
 int main()
 {
